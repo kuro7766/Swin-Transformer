29ffb8b481de60ee3f08bfade4d0b2ff933b2e5c
kuro7766 <49401947+kuro7766@users.noreply.github.com>
2021-11-22 18:34:15 +0800

    add anotation

--- models/swin_mlp.py
+++ models/swin_mlp.py
@@ -59,6 +59,13 @@ def window_reverse(windows, window_size, H, W):
     Returns:
         x: (B, H, W, C)
     """
+
+    """
+    H : height
+    W : width
+    C : channel
+    B : what ? , maybe batch
+    """
     B = int(windows.shape[0] / (H * W / window_size / window_size))
     x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
     x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
